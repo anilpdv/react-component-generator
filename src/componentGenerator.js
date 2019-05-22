a3c748fce5c9c3b56bd5c3a40a1bbd7713ffd3d6
anilpdv <pdvanil007@gmail.com>
2019-05-22 13:09:31 +0530

    modifying code

--- src/componentGenerator.js
+++ src/componentGenerator.js
@@ -1,45 +1,45 @@
-const fs = require("fs");
-let dir = "";
+const fs = require('fs');
+let dir = '';
 
 const createFolder = (name, types) => {
-	dir = `./${name}`;
-	if (!fs.existsSync(dir)) {
-		fs.mkdirSync(dir);
-	}
+  dir = `./${name}`;
+  if (!fs.existsSync(dir)) {
+    fs.mkdirSync(dir);
+  }
 
-	let fileJsx = `${dir}/${name}.component.${types.jstype}`;
-	let fileCss = `${dir}/${name}.component.${types.csstype}`;
+  let fileJsx = `${dir}/${name}.component.${types.jstype}`;
+  let fileCss = `${dir}/${name}.component.${types.csstype}`;
 
-	fs.writeFile(
-		fileJsx,
-		`import react,{Component} from "react";\n
+  fs.writeFile(
+    fileJsx,
+    `import React,{Component} from "react";\n
 import './${name}.component.${types.csstype}';\n
 class ${name} extends Component{\n
-	render(){
-		return(
-			<div>
-			   ${name} component works!
-			</div>);
+	render(){\n
+		return(\n
+			<div>\n
+			   ${name} component works!\n
+			</div>);\n
 		}\n
 	}\n
 export default ${name};`,
 
-		err => {
-			if (err) {
-				console.log(err);
-			}
-			console.log(`file : ${fileJsx} genrated`);
-		}
-	);
+    err => {
+      if (err) {
+        console.log(err);
+      }
+      console.log(`file : ${fileJsx} genrated`);
+    },
+  );
 
-	fs.writeFile(fileCss, "*{padding:0}", err => {
-		if (err) {
-			console.log(err);
-		}
-		console.log(`file : ${fileCss} genrated`);
-	});
+  fs.writeFile(fileCss, '*{padding:0}', err => {
+    if (err) {
+      console.log(err);
+    }
+    console.log(`file : ${fileCss} genrated`);
+  });
 };
 
 module.exports = {
-	createFolder
+  createFolder,
 };
